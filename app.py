2bc36ee30984d7c1dbd428c8c335a3cf84152df6
Zeckszan <96684260+Zeckszan@users.noreply.github.com>
2024-04-23 22:58:09 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,47 @@
 import pyrebase
 from flask import Flask, jsonify
+import re
 
 app = Flask(__name__)
+
+english_to_malay = {
+    'apple': 'epal',
+    'banana': 'pisang',
+    'beetroot': 'bit',
+    'bell pepper': 'lada besar',
+    'cabbage': 'kubis',
+    'capsicum': 'kapsikum',
+    'carrot': 'lobak',
+    'cauliflower': 'kubis bunga',
+    'chilli pepper': 'lada cili',
+    'corn': 'jagung',
+    'cucumber': 'timun',
+    'eggplant': 'terung',
+    'garlic': 'bawang putih',
+    'ginger': 'halia',
+    'grapes': 'anggur',
+    'jalepeno': 'jalepeno',
+    'kiwi': 'kiwi',
+    'lemon': 'limau',
+    'lettuce': 'selada',
+    'mango': 'mangga',
+    'onion': 'bawang besar',
+    'orange': 'oren',
+    'paprika': 'paprika',
+    'pear': 'pir',
+    'peas': 'kacang pea',
+    'pineapple': 'nanas',
+    'pomegranate': 'delima',
+    'potato': 'kentang',
+    'raddish': 'lobak merah',
+    'soy beans': 'kacang soya',
+    'spinach': 'bayam',
+    'sweetcorn': 'jagung manis',
+    'sweetpotato': 'ubi keledek',
+    'tomato': 'tomato',
+    'turnip': 'kubis air',
+    'watermelon': 'tembikai'
+}
 
 @app.route("/")
 def home():
@@ -22,19 +62,42 @@ def filter(productname):
     }
     firebase = pyrebase.initialize_app(config)
     database = firebase.database()
-    data = database.child("users").get()
-    print(data.val())
-    data = data.val()
+    dataJaya = database.child("jaya grocer").get()
+    dataVillage = database.child("village grocer").get()
+    dataLotus = database.child("lotus").get()
+
+    dataJaya = dataJaya.val()
+    dataVillage = dataVillage.val()
+    dataLotus = dataLotus.val()
+
+    print(dataJaya)
+    print(dataVillage)
+    print(dataLotus)
     #data = jsonify(data.val())
 
-    #productname = 'epal'
-
+    productname = "apple"
     print("prod name ",productname)
-    productsreturn = [(product['product_name'], product['price']) for product in data if
-                      str(productname).lower() in product['product_name'].lower()]
+    listJaya =['Jaya Grocer']
+    pattern = re.compile(r'\b{}\b'.format(re.escape(productname)), re.IGNORECASE)
 
-    print("productsreturn ", productsreturn)
-    return jsonify(productsreturn)
+    # Extend the list with the results of list comprehension
+    listJaya.extend([(product['product_name'], product['price']) for product in dataJaya
+                     if re.search(pattern, product['product_name'])])
+    print("jaya",listJaya)
+
+    listVillage =['Village Grocer']
+    listVillage.extend([(product['product_name'], product['price']) for product in dataVillage
+                        if re.search(pattern, product['product_name'])])
+
+    productname = english_to_malay[productname]
+
+    listLotus =['Lotus']
+    listLotus.extend([(product['product_name'], product['price']) for product in dataLotus
+                      if re.search(pattern, product['product_name'])])
+
+    print("productsreturn ", listJaya+listVillage+listLotus)
+    return listJaya+listVillage+listLotus
+    # return jsonify(productsreturn)
 
 
 
